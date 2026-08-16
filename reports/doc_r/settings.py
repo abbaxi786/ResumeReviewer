2a15eeece0a1dce408579fa8559b4fc562d9a2a4
abbaxi786 <141753795+abbaxi786@users.noreply.github.com>
2026-08-16 14:37:49 +0500

    The addition of frontend cors

--- reports/doc_r/settings.py
+++ reports/doc_r/settings.py
@@ -68,6 +68,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://resume-analyser-frontend-lemon.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
